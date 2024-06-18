07c8a3ee94046e3debfc6e1ec3ede4d068ce3b9f
trinhnt75 <trinhlethi888@gmailcom>
2024-06-18 10:43:31 +0700

    update notification

--- FE/src/pages/admin/product/productAdd.tsx
+++ FE/src/pages/admin/product/productAdd.tsx
@@ -1,20 +1,23 @@
-import type { FormProps } from 'antd';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Button, Form, Input, Select } from 'antd';
-import { IProduct } from '../../../interface/Products';
-import { useEffect, useState } from 'react';
-import { ICategory } from '../../../interface/Categories';
+import type { FormProps } from "antd";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Button, Form, Input, Select } from "antd";
+import { IProduct } from "../../../interface/Products";
+import { useEffect, useState } from "react";
+import { ICategory } from "../../../interface/Categories";
+import { toast } from "react-toastify";
 
 const ProductAdd = () => {
   const navigate = useNavigate();
-  
+
   const [cates, setCates] = useState<ICategory[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/api/categories");
+        const response = await axios.get(
+          "http://localhost:3001/api/categories"
+        );
         setCates(response.data?.data || []);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -26,13 +29,13 @@ const ProductAdd = () => {
 
   const dataCates = cates.map((item: ICategory) => ({
     value: item._id,
-    label: item.loai
+    label: item.loai,
   }));
 
-  const onFinish: FormProps<IProduct>['onFinish'] = async (values) => {
+  const onFinish: FormProps<IProduct>["onFinish"] = async (values) => {
     try {
       await axios.post(`http://localhost:3001/api/products/add`, values);
-      alert('Thêm sản phẩm thành công');
+      toast.success("Thêm sản phẩm thành công");
       console.log(values);
       navigate("/admin/product");
     } catch (err) {
@@ -40,8 +43,8 @@ const ProductAdd = () => {
     }
   };
 
-  const onFinishFailed: FormProps<IProduct>['onFinishFailed'] = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+  const onFinishFailed: FormProps<IProduct>["onFinishFailed"] = (errorInfo) => {
+    console.log("Failed:", errorInfo);
   };
 
   return (
@@ -57,46 +60,50 @@ const ProductAdd = () => {
       <Form.Item
         label="Tên sản phẩm"
         name="name"
-        rules={[{ required: true, message: 'Vui lòng nhập tên sản phẩm!' }]}
+        rules={[{ required: true, message: "Vui lòng nhập tên sản phẩm!" }]}
       >
         <Input />
       </Form.Item>
       <Form.Item
         label="Giá"
         name="price"
-        rules={[{ required: true, message: 'Vui lòng nhập giá sản phẩm!' }]}
+        rules={[{ required: true, message: "Vui lòng nhập giá sản phẩm!" }]}
       >
         <Input type="number" />
       </Form.Item>
       <Form.Item
         label="Danh mục"
         name="categoryId"
-        rules={[{ required: true, message: 'Vui lòng chọn danh mục!' }]}
+        rules={[{ required: true, message: "Vui lòng chọn danh mục!" }]}
       >
         <Select
           placeholder="Chọn danh mục"
-          style={{ width: '100%' }}
+          style={{ width: "100%" }}
           options={dataCates}
         />
       </Form.Item>
       <Form.Item
         label="Số lượng"
         name="quantity"
-        rules={[{ required: true, message: 'Vui lòng nhập số lượng sản phẩm!' }]}
+        rules={[
+          { required: true, message: "Vui lòng nhập số lượng sản phẩm!" },
+        ]}
       >
         <Input type="number" />
       </Form.Item>
       <Form.Item
         label="Ảnh"
         name="image"
-        rules={[{ required: true, message: 'Vui lòng nhập đường dẫn ảnh sản phẩm!' }]}
+        rules={[
+          { required: true, message: "Vui lòng nhập đường dẫn ảnh sản phẩm!" },
+        ]}
       >
         <Input />
       </Form.Item>
       <Form.Item
         label="Mô tả"
         name="description"
-        rules={[{ required: true, message: 'Vui lòng nhập mô tả sản phẩm!' }]}
+        rules={[{ required: true, message: "Vui lòng nhập mô tả sản phẩm!" }]}
       >
         <Input.TextArea rows={4} />
       </Form.Item>
