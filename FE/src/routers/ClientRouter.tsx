b722205f532ac6915e5d3478ca9b62c9495a7550
trinhnt75 <trinhlethi888@gmailcom>
2024-06-25 23:10:01 +0700

    feat: HTML payment

--- FE/src/routers/ClientRouter.tsx
+++ FE/src/routers/ClientRouter.tsx
@@ -5,7 +5,7 @@ import Home from "../pages/client/Home";
 import Product from "../pages/client/Product";
 import Footer from "../layout/client/Footer";
 import ClientLogin from "../pages/client/ClientLogin";
-// import Payment from "../pages/client/Payment";
+import Payment from "../pages/client/Payment";
 import CartPage from "../pages/client/Cart";
 import ProductDetail from "../pages/client/ProductDetail";
 
@@ -20,7 +20,7 @@ export default function ClientRouter() {
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<CartPage/>} />
         <Route path="/login" element={<ClientLogin />} />
-        {/* <Route path="/payment" element={<Payment />} /> */}
+        <Route path="/payment" element={<Payment />} />
       </Routes>
       <Footer />
     </div>
