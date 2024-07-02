1a715186c1d04f3df96711fb4b98b6d2f970770d
tuanzang <tuanldph26359@fpt.edu.vn>
2024-07-02 19:39:32 +0700

    update

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -3,20 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import ClientRouter from "./routers/ClientRouter";
 import AdminRouter from "./routers/AdminRouter";
 import NotFound404 from "./routers/NotFound404";
-<<<<<<< HEAD
-=======
-
 import PrivateRouter from "./components/PrivateRouter";
->>>>>>> 1d77e75b122fbe7c8fc4f114b72264039c009b79
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/*" element={<ClientRouter />} />
-<<<<<<< HEAD
-        <Route path="/admin/*" element={<AdminRouter />} />
-=======
         <Route
           path="/admin/*"
           element={
@@ -26,7 +19,6 @@ export default function App() {
           }
         />
         {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
->>>>>>> 1d77e75b122fbe7c8fc4f114b72264039c009b79
         <Route path="*" element={<NotFound404 />} />
       </Routes>
     </div>
