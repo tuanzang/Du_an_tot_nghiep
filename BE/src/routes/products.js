909cf4e6fd20e1646c954a1b36229624ebc101ac
Dương Đức Long <duongduclong4@gmail.com>
2024-07-27 11:31:00 +0700

    update create product size

--- BE/src/routes/products.js
+++ BE/src/routes/products.js
@@ -19,12 +19,12 @@ productRouter.get("/", getAllProduct);
 productRouter.get("/search", searchProducts);
 productRouter.get("/:id", getDetailProduct);
 productRouter.post("/add", createProduct);
-productRouter.post("/add/size", createProductSizes);
+productRouter.post("/:productId/add/size", createProductSizes);
 productRouter.get("/productSize/:idProduct", getProductSizesByProductId);
 productRouter.put("/:id", updateProduct);
 productRouter.put("/productSize/:id", updateProductSizes);
 productRouter.delete("/:id", deleteProduct);
-productRouter.get('/filter/price', filterProductsByPrice);
-productRouter.get('/filter/category/:categoryId', filterProductByCategory);
+productRouter.get("/filter/price", filterProductsByPrice);
+productRouter.get("/filter/category/:categoryId", filterProductByCategory);
 
 export default productRouter;
